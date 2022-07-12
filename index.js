db8e7cc942533d8fd5446f077d440156b86883e1
Emilio Escobar <emilio.escobar@gmail.com>
2022-07-12 12:45:01 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const logger = require('./loggerMiddleware')
+app.use(cors())
+app.use(express.json())
+app.use(logger)
+let notes = [
+  {
+    id: 1,
+    content: 'Me tengo que suscribir a @midudev en YouTube',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Tengo que estudiar las clases del FullStack Bootcamp',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'Repasar los retos de JS de midudev',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+app.get('/', (request, response) => {
+  response.send('<h1>Hola mundo</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(e => e.id === id)
+  response.send(note)
+})
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.filter(e => e.id !== id)
+  response.send(note)
+})
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log(note)
+
+  if (!note || !note.content) {
+    return response.status(400).json({
+      error: 'note.content is missing'
+    })
+  }
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+  }
+  notes = [...notes, newNote]
+
+  response.status(201).json(newNote)
+})
+
+app.use((request, response) => {
+  // console.log(request.path)
+  response.status(404).json({
+    error: 'Not found'
+  })
+})
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}`)
+})
